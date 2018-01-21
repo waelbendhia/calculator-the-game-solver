c2244605c914ae90137cc4c12dfb0b1d949aaf49
Wael Ben Dhia <wael@localhost.localdomain>
2018-01-21 23:29:38 +0100

    Add Inv10 action
    Add Inv10 parsing and application
    Fix Replace application

--- src/Main.hs
+++ src/Main.hs
@@ -23,9 +23,10 @@ data Action
   | Mirror
   | Store
   | Unstore
+  | Inv10
   | Powers Integer
-  | Replace Integer
-            Integer
+  | Replace String
+            String
   deriving (Show)
 
 data GameState = GameState
@@ -39,21 +40,20 @@ data GameState = GameState
 addToAction :: Integer -> Action -> Action
 addToAction n act =
   let transform m =
-        if m < 0
-          then m - n
-          else m + n
+        if | m < 0 -> m - n
+           | otherwise -> m + n
   in case act of
        Add x -> Add $ transform x
        Multiply x -> Multiply $ transform x
        Insert x -> Insert $ transform x
        Divide x -> Divide $ transform x
        Powers x -> Powers $ transform x
-       Replace x y -> Replace (transform x) (transform y)
+       Replace x y ->
+         Replace (show $transform $ read x) (show $ transform $read y)
        x -> x
 
-replaceInt :: (Show a4, Show a3, Show a2, Read a1) => a2 -> a3 -> a4 -> Maybe a1
-replaceInt from with val =
-  maybeRead $ replace (show from) (show with) (show val)
+replaceInt :: (Show a2, Read a1) => String -> String -> a2 -> Maybe a1
+replaceInt from with = maybeRead . (replace from with) . show
 
 reverseInt :: Integral t => t -> t
 reverseInt n = aux n 0
@@ -67,10 +67,9 @@ applyAction :: Action -> GameState -> Maybe GameState
 applyAction act g =
   let GameState {currentValue = x} = g
       replaceValue y =
-        if y > 99999
-          then Nothing
-          else Just $
-               g {remainingMoves = remainingMoves g - 1, currentValue = y}
+        if | y > 99999 -> Nothing
+           | otherwise ->
+             Just $ g {remainingMoves = remainingMoves g - 1, currentValue = y}
   in case act of
        Add y -> replaceValue $ x + y
        Multiply y -> replaceValue $ x * y
@@ -87,27 +86,31 @@ applyAction act g =
          (readMaybe $ last (show x) : init (show x)) >>= replaceValue
        Sum ->
          replaceValue $
-         (if x < 0
-            then (*) (-1)
-            else (*) 1) $
+         (*)
+           (if | x < 0 -> -1
+               | otherwise -> 1) $
          toInteger $ sum $ map digitToInt $ filter isDigit $ show x
        Divide y ->
-         if x `rem` y == 0
-           then replaceValue $ x `quot` y
-           else Nothing
+         if | x `rem` y == 0 -> replaceValue $ x `quot` y
+            | otherwise -> Nothing
        AddToButtons n ->
          Just $
          g
          { remainingMoves = remainingMoves g - 1
          , actions = map (addToAction n) (actions g)
          }
+       Inv10 ->
+         replaceValue $
+         read $
+         show x >>= \c ->
+           if | isDigit c -> show $ 10 - digitToInt c
+              | otherwise -> [c]
        Store ->
          case store g of
            Nothing -> Just $ g {store = Just x}
            Just s ->
-             if x == s
-               then Nothing
-               else Just $g {store = Just x}
+             if | x == s -> Nothing
+                | otherwise -> Just $ g {store = Just x}
        Unstore ->
          case store g of
            Nothing -> Nothing
@@ -184,22 +187,23 @@ promptInt prompt = do
 promptActions :: IO [Action]
 promptActions = do
   ln <- promptLine "Action or done for done"
-  if ln == "done"
-    then return []
-    else case actionFromString ln of
-           Just x -> do
-             acts <- promptActions
-             return $
-               case x of
-                 Store -> [Store, Unstore] ++ acts
-                 _ -> x : acts
-           Nothing -> do
-             putStrLn "Could not parse actions"
-             promptActions
+  if | ln == "done" -> return []
+     | otherwise ->
+       case actionFromString ln of
+         Just x -> do
+           acts <- promptActions
+           return $
+             case x of
+               Store -> [Store, Unstore] ++ acts
+               _ -> x : acts
+         Nothing -> do
+           putStrLn "Could not parse actions"
+           promptActions
 
 actionFromString :: [Char] -> Maybe Action
 actionFromString str
   | length str == 0 = Nothing
+  | map toLower str == "inv10" = Just Inv10
   | map toLower str == "store" = Just Store
   | map toLower str == "<shift" = Just ShiftLeft
   | map toLower str == "shift>" = Just ShiftRight
@@ -210,12 +214,12 @@ actionFromString str
   | str == "<<" = Just RemoveDigit
   | isInfixOf "=>" str =
     let operands = split "=>" str
-    in (if length operands == 2
-          then (let [n, m] = map readMaybe operands
-                in (do n' <- n
-                       m' <- m
-                       return (Replace n' m')))
-          else Nothing)
+    in if | length operands == 2 ->
+            (let [n, m] = operands
+             in (do _ <- readMaybe n :: Maybe Integer
+                    _ <- readMaybe m :: Maybe Integer
+                    return (Replace n m)))
+          | otherwise -> Nothing
   | head str == '-' = readMaybe str >>= Just . Add
   | head str == '+' = readMaybe (tail str) >>= Just . Add
   | head str == '^' = readMaybe (tail str) >>= Just . Powers
